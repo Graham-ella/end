a42ca06b81d401ded2894bcc79a002cee5630b03
Graham <3235962608@qq.com>
2023-02-03 19:57:06 +0800

    图上的Person点从后端读取

--- src/main/java/com/example/endcase/controller/PersonController.java
+++ src/main/java/com/example/endcase/controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.example.endcase.controller;
 
+import com.example.endcase.pojo.CustomNode;
 import com.example.endcase.pojo.Person;
 import com.example.endcase.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/person")
 public class PersonController {
@@ -15,7 +19,18 @@ public class PersonController {
     PersonService personService;
 
     @GetMapping("/{name}")
-    public Person findByName(@PathVariable("name") String name){
-        return personService.findByName(name);
+    public CustomNode findByName(@PathVariable("name") String name){
+        Person person = personService.findByName(name);
+        return new CustomNode(person.getName(), 1);
+    }
+
+    @GetMapping("/all")
+    public List<CustomNode> findAllPerson(){
+        List<Person> personList = personService.findAllPerson();
+        List<CustomNode> customNodeList = new ArrayList<CustomNode>();
+        for(Person person: personList){
+            customNodeList.add(new CustomNode(person.getName(), 1));
+        }
+        return customNodeList;
     }
 }
